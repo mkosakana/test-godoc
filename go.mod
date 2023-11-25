5e6f5785fa9d354a4e14968ba8ff7be0707c5d86
mkosakana <mkosakana.dev@gmail.com>
2023-11-26 02:16:50 +0900

    add: add test package

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mkosakana/test-godoc
+
+go 1.21.2
---
+++ test.go
@@ -0,0 +1,24 @@
+// Package test is a test package for Godoc
+package test
+
+// Test
+type Test struct{}
+
+// test
+type test struct{}
+
+// TestVar
+var TestVar int
+
+// testVar
+var testVar int
+
+// TestFn
+func TestFn() error {
+	return nil
+}
+
+// testFn
+func testFn() error {
+	return nil
+}
